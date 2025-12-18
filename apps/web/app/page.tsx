d7478100a7f95eb7e1c4bd493f7db765d627f9d6
Pranav Gupta <pranavg9876@gmail.com>
2025-12-18 12:05:43 +0530

    feat: integrate Convex backend and update frontend to use new API

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,13 @@
-import { Button } from "@workspace/ui/components/button"
-import { add } from "@workspace/math/add"
+import { useQuery } from "convex/react"
+import { api } from "@workspace/backend/convex/_generated/api"
 
 export default function Page() {
+
+  const users = useQuery(api.users.getMany)
   return (
     <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello app/works</h1>
-        <Button size="sm">Button</Button>
-        <p>{add(2,3)}</p>
-      </div>
+      <p>app/webs</p>
+      {JSON.stringify(users)}
     </div>
   )
 }
