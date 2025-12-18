b29ddd8c6d9fdf84f135418715d4ddb713402258
Pranav Gupta <pranavg9876@gmail.com>
2025-12-18 13:17:02 +0530

    feat: add user mutation and update page components to include user addition functionality

--- packages/backend/convex/users.ts
+++ packages/backend/convex/users.ts
@@ -1,10 +1,20 @@
-import { query } from "./_generated/server"
+import { query, mutation } from "./_generated/server";
 
 export const getMany = query({
-    args: {},
-    handler: async (ctx) => {
-       const users = await ctx.db.query("users").collect()
+  args: {},
+  handler: async (ctx) => {
+    const users = await ctx.db.query("users").collect();
 
-       return users;
-    }
-})
+    return users;
+  },
+});
+
+export const add = mutation({
+  args: {},
+  handler: async (ctx) => {
+    const userId = await ctx.db.insert("users", {
+      name: "Pranav",
+    });
+    return userId;
+  },
+});
